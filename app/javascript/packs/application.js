cda15b2b102c90156a0402e975b0a4b70e2a3d07
Rakesh Singh <rkushwah@bestpeers.com>
2021-05-23 11:47:57 +0530

    add bootstrap in project

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,11 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+ // import the bootstrap javascript module
+ import "bootstrap"
+ // import the application.scss we created for the bootstrap CSS (if you are not using assets stylesheet)
+ import "../../assets/stylesheets/application"
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
